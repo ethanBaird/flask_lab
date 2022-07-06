3e8ea26bf1922de4c9c106c04cebf3c12a307818
Ethan <ethanbaird94@gmail.com>
2022-07-06 14:30:09 +0100

    GET one works

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -6,3 +6,6 @@ from models.order_list import orders
 def index():
     return render_template('index.html', orders=orders)
 
+@app.route('/orders/<order_index>')
+def order(order_index):
+    return render_template('order.html', order=orders[int(order_index)])
