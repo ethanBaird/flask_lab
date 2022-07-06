f560170e8c14c7bd5d28efbc9927b0d7f3fc8f8b
Ethan <ethanbaird94@gmail.com>
2022-07-06 13:32:07 +0100

    added class order

---
+++ models/order.py
@@ -0,0 +1,7 @@
+class Order:
+
+    def __init__ (self, _customer_name, _order_date, _quantity):
+        self.customer_name = _customer_name
+        self.order_date = _order_date
+        self.quanity = _quantity
+
