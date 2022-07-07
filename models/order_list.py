34c350ab80e727e99f198f0e0be8306f37893fd4
Ethan <ethanbaird94@gmail.com>
2022-07-07 08:29:07 +0100

    tidied up css on home page

--- models/order_list.py
+++ models/order_list.py
@@ -2,5 +2,6 @@ from models.order import Order
 
 order1 = Order("Carly", "6/7/22", 6)
 order2 = Order("Ethan", "23/2/21", 15)
-orders = [order1, order2]
+order3 = Order('Rachel', '15/04/22', 13)
+orders = [order1, order2, order3]
 
